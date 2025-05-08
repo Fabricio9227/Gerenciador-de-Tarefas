f7fba8b5d2e155bbd6edfb42a4136445ca1fa49d
fabriciomarthinsdasilva@gmail.com <10089799@pucrs.br>
2025-05-08 17:47:56 -0300

    primeiro commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("Olá mundo ");
+  return (
+    <div>
+      <h1>{message}</h1>
+      <button
+        onClick={() => {
+          setMessage("Fui clicado");
+        }}
+      >
+        Button
+      </button>
+    </div>
+  );
+}
+
+export default App;
